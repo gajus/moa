05d640c035f6182f2dd6ddf88687e3fc1453425d
Gajus Kuizinas <g.kuizinas@anuary.com>
2014-04-27 01:22:26 +0000

    Minor bug fixes.

---
+++ tests/MotherTest.php
@@ -0,0 +1,22 @@
+<?php
+class MotherTest extends PHPUnit_Framework_TestCase {
+    private
+        $db;
+
+    public function setUp () {
+        $this->db = new \PDO('mysql:dbname=moa', 'travis');
+        $this->db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+        $this->db->exec("TRUNCATE TABLE `datetime`");
+        $this->db->exec("TRUNCATE TABLE `duplicate`");
+        $this->db->exec("TRUNCATE TABLE `greedy`");
+        $this->db->exec("TRUNCATE TABLE `greedy_timestamp`");
+        $this->db->exec("TRUNCATE TABLE `number`");
+        $this->db->exec("TRUNCATE TABLE `string`");
+    }
+
+    public function testGetDatabaseHandle () {
+        $foo = new \Sandbox\Model\String($this->db);
+
+        $this->assertSame($this->db, $foo->getDatabaseHandle());
+    }
+}
